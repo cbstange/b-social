d3138156ed3a63aabc8340b9f8ca18703f313a10
cbstange <>
2024-01-29 05:10:27 +0000

    Add signin & logout function to views.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -4,4 +4,6 @@ from my_project import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
 ]
--- my_project/views.py
+++ my_project/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+
+@Login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -36,3 +39,25 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Login details not found.')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+@Login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
